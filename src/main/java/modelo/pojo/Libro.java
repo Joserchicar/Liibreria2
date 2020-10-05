f57770c38ceb19c8f0535013e5971a7bfcedefb1
javaee <javaee@javaee-VirtualBox>
2020-10-05 14:02:12 +0200

    limpieza  codigo

--- src/main/java/modelo/pojo/Libro.java
+++ src/main/java/modelo/pojo/Libro.java
@@ -6,72 +6,67 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.NotBlank;
+
 /**
- *  Libro.java  pojo del objeto libro
+ * Libro.java pojo del objeto libro
+ * 
  * @author joserra
  * @Version 1.0
- * @see modelo.pojoGenero sobre genros de los libros
- * @ see Usuario usuario a quien pertenece el libro
+ * @see Genero sobre generos de los libros
+ * @see Usuario usuario a quien pertenece el libro
  * 
  */
 public class Libro {
-	
-		private int id;
-		
-		@NotBlank
-		@Size( min = 3, max = 100, message = "La longtitud de ser entre 3 y 100 caracteres")
-		private String titulo;
-		
-		@NotBlank ( message = "Escribe la url de la imagen")
-		private String  imagen;
-		
-		@Min( value = 0, message = "Debe ser positivo")
-		private float precio; 
-		
-		private Date fechaCreacion;
-		private Date fechaValidacion;
-		
-		private Usuario usuario;
-		
-		private Genero genero;
-		
 
-	
+	private int id;
+
+	@NotBlank
+	@Size(min = 3, max = 100, message = "La longtitud de ser entre 3 y 100 caracteres")
+	private String titulo;
+
+	@NotBlank(message = "Escribe la url de la imagen")
+	private String imagen;
+
+	@Min(value = 0, message = "Debe ser positivo")
+	private float precio;
+
+	private Date fechaCreacion;
+	private Date fechaValidacion;
+
+	private Usuario usuario;
+
+	private Genero genero;
+
 	public Libro() {
 		super();
 		this.id = 0;
 		this.titulo = "";
 		this.imagen = "https://picsum.photos/100/100";
 		this.precio = 0;
-		this.usuario=new Usuario();
-		this.genero=new Genero();
-		
+		this.usuario = new Usuario();
+		this.genero = new Genero();
+
 	}
 
 	public Libro(String titulo) {
 		this();
 		this.titulo = titulo;
 	}
-	
-	
 
 	public int getId() {
 		return id;
 	}
 
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 
 	public String getTitulo() {
 		return titulo;
 	}
 
-
 	public void setTitulo(String titulo) {
-		this.titulo= titulo;
+		this.titulo = titulo;
 	}
 
 	public String getImagen() {
@@ -94,22 +89,18 @@ public class Libro {
 		return genero;
 	}
 
-
 	public void setGenero(Genero genero) {
 		this.genero = genero;
 	}
 
-	
-	
 	public Usuario getUsuario() {
 		return usuario;
 	}
 
 	public void setUsuario(Usuario usuario) {
-		this.usuario=usuario;
-		
-	}
+		this.usuario = usuario;
 
+	}
 
 	public Date getFechaCreacion() {
 		return fechaCreacion;
@@ -134,9 +125,4 @@ public class Libro {
 				+ ", genero=" + genero + "]";
 	}
 
-	
-
-	
-	
-	
 }
